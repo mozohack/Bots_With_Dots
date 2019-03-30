30114e02dcc504c27ed96b7505b3a9e8092eaf85
Alapan Kar <alapankar27@gmail.com>
2019-03-30 23:34:21 +0530

    Applied ML prediction algorithms

--- xy.py
+++ xy.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
 l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
 	'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
 	'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
