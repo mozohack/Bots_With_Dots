e205f620d71486351ab9c5640f11adf1c645d54b
Sukrit Bhattacharya <30416095+sukritbhattacharya@users.noreply.github.com>
2019-03-31 14:35:21 +0530

    UI updates

---
+++ alter.py
@@ -0,0 +1,21 @@
+def alter(string1):
+	if string1=="(vertigo) Paroymsal  Positional Vertigo":
+		return "Benign paroxysmal positional vertigo"
+	elif string1=="Chronic cholestasis":			
+		return "Cholestatis"
+	elif string1=="Dimorphic hemmorhoids(piles)":			
+		return "Hemorrhoid"
+	elif string1=="Fungal infection":			
+		return "Mycosis"
+	elif string1=="Osteoarthristis":			
+		return "Osteoarthritis"
+	elif string1=="Paralysis (brain hemorrhage)":			
+		return "Intracerebral hemorrhage"
+	elif string1=="Peptic ulcer diseae":			
+		return "Peptic ulcer disease"	
+	elif string1=="Drug Reaction":
+		return "Adverse Drug Reaction"
+	else:
+		return string1		
+		
+
